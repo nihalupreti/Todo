25c3287f9fdd517bedab00edeb70238ac66e8005
Nihal upreti <n.ytuchiha@gmail.com>
2023-10-01 22:03:50 +0545

    implementes presistant login

--- home/views.py
+++ home/views.py
@@ -6,20 +6,27 @@ from .models import Data
 
 
 def login_page(request):
-    if (request.method) == "POST":
-        username_input = request.POST["username"]
-        password_input = request.POST["password"]
-        user = authenticate(request, username=username_input,
-                            password=password_input)
-        if user is not None:
-            login(request, user)
-            request.session["is_logged_in"] = True
-            appropriate_user = Data.objects.get(email=user.email).id
-            url_const = f"/todo/{appropriate_user}"
-            return HttpResponseRedirect(url_const)
-        else:
-            return None
-    return render(request, "home/login.html")
+    url_const = ""
+    my_user = request.user
+    # If you want to know if the user is logged in
+    if (my_user.is_authenticated):
+        print("succes")
+        return HttpResponseRedirect(f"/todo/{Data.objects.get(email=my_user.email).id}")
+    else:
+        if (request.method) == "POST":
+            username_input = request.POST["username"]
+            password_input = request.POST["password"]
+            user = authenticate(request, username=username_input,
+                                password=password_input)
+            if user is not None:
+                login(request, user)
+                request.session["is_logged_in"] = True
+                appropriate_user = Data.objects.get(email=user.email).id
+                url_const = f"/todo/{appropriate_user}"
+                return HttpResponseRedirect(url_const)
+            else:
+                return None
+        return render(request, "home/login.html")
 
 
 def todo_page(request, uuid):
