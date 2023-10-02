38b276f423a5da5450ad5c9b81b398ab6ed72b1e
Nihal upreti <n.ytuchiha@gmail.com>
2023-10-02 21:07:16 +0545

    fixed IDORS, implemented user register and logout feature

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_page),
+    path("", views.login_page, name="login-page"),
+    path("register", views.register_user_page, name="register-user"),
+    path("logout", views.logout_view, name="logout-user"),
     path("todo/<uuid>", views.todo_page)
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from .models import Data
 #
 
 
+def register_user_page(request):
+    if (request.method) == "POST":
+        user_username = request.POST["username"]
+        user_email = request.POST["email"]
+        user_password = request.POST["password"]
+        user = User.objects.create_user(
+            user_username, user_email, user_password)
+        user.save()
+        return HttpResponseRedirect("/")
+    else:
+        return render(request, "home/register.html")
+
+
 def login_page(request):
-    url_const = ""
     my_user = request.user
     # If you want to know if the user is logged in
     if (my_user.is_authenticated):
-        print("succes")
         return HttpResponseRedirect(f"/todo/{Data.objects.get(email=my_user.email).id}")
     else:
         if (request.method) == "POST":
@@ -29,9 +42,20 @@ def login_page(request):
         return render(request, "home/login.html")
 
 
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+
+
+@login_required
 def todo_page(request, uuid):
-    user_data = Data.objects.all().get(id=uuid)
-    todo_lists = user_data.todos["unfinished"]
-    return render(request, "home/todo-page.html", {
-        "tasks": todo_lists
-    })
+    user_data = Data.objects.get(id=uuid)
+    # print(f"{user_data.email} data database")
+    # print(f"{request.user.username} user database")
+    if request.user.email == user_data.email:
+        todo_lists = user_data.todos["unfinished"]
+        return render(request, "home/todo-page.html", {
+            "tasks": todo_lists
+        })
+    else:
+        return HttpResponseForbidden("Access Denied")
