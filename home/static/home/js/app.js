6f11a05192c81f211765c308f8f689bc958d69a4
Nihal upreti <n.ytuchiha@gmail.com>
2023-10-10 13:03:04 +0545

    add a task implementation

--- home/static/home/js/app.js
+++ home/static/home/js/app.js
@@ -1,26 +1,89 @@
 $(document).ready(function () {
-  // Add a click event listener to checkboxes
   $(".todo__state").on("click", function () {
-    // Get the task ID from the data attribute
-    // const taskId = $(this).closest(".todo__wrapper").data("task-id");
+    const taskText = $(this)
+      .closest(".todo__wrapper")
+      .find(".todo__text")
+      .text();
 
     // Send an AJAX request to delete the task
     $.ajax({
       url: "/delete-task",
       type: "POST",
-      // data: { task_id: taskId },
+      data: { user_todo: taskText },
       headers: {
         "X-CSRFToken": csrfToken,
       },
       success: function (response) {
         if (response.status) {
           console.log(response);
-          // window.location.href = response.redirect;
         }
       },
       error: function (error) {
         console.error("Error deleting task:", error);
       },
+    });
+  });
+
+  // Add a new task when "Add Task" button is clicked
+  $(".add-task-button").on("click", function () {
+    const newTaskHtml = `
+      <label class="todo">
+        <div class="todo__wrapper">
+          <input class="todo__state" type="checkbox" />
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">
+            <use xlink:href="#todo__line" class="todo__line"></use>
+            <use xlink:href="#todo__box" class="todo__box"></use>
+            <use xlink:href="#todo__check" class="todo__check"></use>
+            <use xlink:href="#todo__circle" class="todo__circle"></use>
+          </svg>
+          <input class="todo__text-input" type="text" placeholder="Enter your task..." />
+        </div>
+      </label>
+    `;
+
+    const $newTask = $(newTaskHtml);
+    $(".todo-list").append($newTask);
+
+    // Focus on the input field of the newly added task
+    const $textInput = $newTask.find(".todo__text-input");
+    $textInput.focus();
+
+    // Handle saving a task when Enter is pressed or focus is lost
+    $textInput.on("keydown blur", function (e) {
+      if (e.type === "keydown" && e.key === "Enter") {
+        e.preventDefault();
+
+        const taskText = $(this).val();
+        if (taskText.trim() === "") {
+          // If the task text is empty, remove the task
+          $(this).closest(".todo").remove();
+        } else {
+          // Replace the input field with the task text
+          $(this).replaceWith(`<div class="todo__text">${taskText}</div>`);
+          $.ajax({
+            url: "/save-task",
+            type: "POST",
+            data: { added_todo: taskText },
+            headers: {
+              "X-CSRFToken": csrfToken,
+            },
+            success: function (response) {
+              if (response.status) {
+                console.log(response);
+              }
+            },
+            error: function (error) {
+              console.error("Error deleting task:", error);
+            },
+          });
+        }
+      } else if (e.type === "blur") {
+        const taskText = $(this).val();
+        if (taskText.trim() === "") {
+          // If the input field loses focus and is empty, remove the task
+          $(this).closest(".todo").remove();
+        }
+      }
     });
   });
 });
--- home/views.py
+++ home/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseForbidden, JsonResponse, HttpResponse
 from .models import Data
-#
 
 
 def register_user_page(request):
@@ -36,6 +35,7 @@ def login_page(request):
                 login(request, user)
                 request.session["is_logged_in"] = True
                 appropriate_user = Data.objects.get(email=user.email).id
+                request.session["id"] = str(appropriate_user)
                 url_const = f"/todo/{appropriate_user}"
                 return HttpResponseRedirect(url_const)
             else:
@@ -56,7 +56,8 @@ def todo_page(request, uuid):
     if request.user.email == user_data.email:
         todo_lists = user_data.todos["unfinished"]
         return render(request, "home/todo-page.html", {
-            "tasks": todo_lists
+            "tasks": todo_lists,
+            "user_id": uuid
         })
     else:
         return HttpResponseForbidden("Access Denied")
@@ -64,7 +65,30 @@ def todo_page(request, uuid):
 
 def delete_data(request):
     if (request.method) == "POST":
-        # table_data = Data.objects.all()
+        # for key, value in request.session.items():
+        #     print(f"Session Key: {key}, Value: {value}")
+        table_data = Data.objects.get(id=request.session.get("id"))
+        todos = table_data.todos
+        clicked_task = request.POST.get('user_todo')
+        todos["finished"].append(clicked_task)
+        todos["unfinished"].remove(clicked_task)
+        # update the database
+        table_data.todos = todos
+        table_data.save()
+        table_data.save()
+        response_data = {
+            "status": True,
+        }
+        return JsonResponse(response_data)
+    else:
+        return HttpResponse("Method not allowed", status=405)
+
+
+def save_task(request):
+    if (request.method) == "POST":
+        table_data = Data.objects.get(id=request.session.get("id"))
+        table_data.todos["unfinished"].append(request.POST.get('added_todo'))
+        table_data.save()
         response_data = {
             "status": True,
         }
